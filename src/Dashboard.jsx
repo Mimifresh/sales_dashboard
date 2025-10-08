4243082723b2b1f7a4c1e961319c23de1110b86e
Chukwu Miracle <mimigolld@gmail.com>
2025-10-08 22:13:56 +0100

    added real time update of the dash board

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,34 +1,107 @@
 import supabase from "./supabase-client";
 import { useEffect, useState } from "react";
-import { Charts } from "react-charts";
+import { Chart } from "react-charts";
 function Dashboard() {
 
   const [matrix, setMatrix] = useState([])
 
     useEffect(()=>{
         fetchMatrix()
+        const channel = supabase
+        .channel('deal-changes')
+        .on(
+        'postgres_changes',
+        { 
+          event: '*', 
+          schema: 'public', 
+          table: 'sales_deals'  
+        },
+        (payload) => {
+          fetchMetrics();
+        })
+      .subscribe();
+ 
+    // Clean up subscription
+    return () => {
+      supabase.removeChannel(channel);
+    };
     }, [])
 
 
-    async function fetchMatrix(){
-    const { data, error} = await supabase
-    .from('sales_deals')
-    .select(
-      `
-      name,
-      value.sum()
-      `,
-    )
-    if (error) {
-      throw error
+	async function fetchMatrix() {
+    try {
+      const { data, error } = await supabase
+        .from('sales_deals')
+        .select(
+          `
+          name,
+          value.sum()
+          `,
+        )
+      if (error) {
+        throw error;
+      }
+      setMatrix(data);
+    } catch (error) {
+        console.error('Error fetching metrics:', error);
     }
-    console.log(data)
-    setMatrix(data)
+  }
+
+  const chartData = [
+    {
+      data: matrix.map((m) => ({
+        primary: m.name,
+        secondary: m.sum,
+      })),
+    },
+  ];
+
+  const primaryAxis = {
+    getValue: (d) => d.primary,
+    scaleType: 'band',
+    padding: 0.2,
+    position: 'bottom',
+  };
+
+  function y_max() {
+    if (matrix.length > 0) {
+      const maxSum = Math.max(...matrix.map((m) => m.sum));
+      return maxSum + 2000;
     }
+    return 5000; 
+  }
+
+  const secondaryAxes = [
+    {
+      getValue: (d) => d.secondary,
+      scaleType: 'linear',
+      min: 0,
+      max: y_max(),
+      padding: {
+        top: 20,
+        bottom: 40,
+      },
+    },
+  ];
+
   return (
     <div className="dashboard-wrapper">
       <div className="chart-container">
         <h2>Total Sales This Quarter ($)</h2>
+        <div style={{ flex: 1 }}>
+          <Chart
+            options={{
+              data: chartData,
+              primaryAxis,
+              secondaryAxes,
+              type: 'bar',
+              defaultColors: ['#58d675'],
+              tooltip: {
+                show: false,
+              },
+            }}
+          />
+        </div>
       </div>
     </div>
   );
