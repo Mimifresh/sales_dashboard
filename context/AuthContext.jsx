a37d98b9255152b0ff3c83afdeea554dc71dde65
Chukwu Miracle <mimigolld@gmail.com>
2025-10-13 15:03:21 +0100

    error handling for fetching session data

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,10 +1,26 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import supabase from "../src/supabase-client";
+
 
 const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) =>{
     const [ session, setSession ] = useState(undefined)
 
+
+    useEffect(()=>{
+        async function getInitialSession(){
+            try{
+                const { data, error } = await supabase.auth.getSession()
+                if(error) throw error
+                console.log("Initial session:", data.session)
+                setSession(data.session)
+            }
+            catch(error){
+            console.error("Error getting initial session:", error.message)
+        }
+    } getInitialSession()
+    },[])
     return(
         <AuthContext.Provider value={{session}}>
             {children}
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -4,6 +4,8 @@ import {useAuth} from '../../context/AuthContext.jsx';
 const Signin = () => {
     const { session } = useAuth();
     console.log(session);
+
+    
     return(
         <>
             <h1 className="landing-header">Signin Page</h1>
